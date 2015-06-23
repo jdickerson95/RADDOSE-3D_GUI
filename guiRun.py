e07c6ad40dea13e0c2b1221d61de298dccf4866c
unknown <lina2532@efg7.bioch.ox.ac.uk>
2015-06-23 16:22:35 +0100

    fixed poorly merged code - no repeated refreshing of experiment and strategy lists now

--- guiRun.py
+++ guiRun.py
@@ -739,20 +739,11 @@ class RADDOSEgui(Frame):
 			"Experiment name %s already exists. Do you want to overwrite this experiment?"%(expName))
 			if addQuery == 'yes':
 				self.runStrategy()
-				
-				#Update the experiment list in the strategy window
-				self.addToExperimentList()
 			else:
 				pass
 		else:
 			os.mkdir(expName)
 			self.runStrategy()
-
-			#Update the experiment list in the strategy window
-			self.addToExperimentList()
-
-		#Update experiments loaded to summary window
-		self.refreshExperimentChoices()
 
 	def runStrategy(self):
 		"""Run RADDOSE-3D given specified crystal, beam and wedge objects
