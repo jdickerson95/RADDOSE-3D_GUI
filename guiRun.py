8ec8c4d53a7faee149f3c121abf4927b5171b98b
Charlie Bury <csbury@me.com>
2015-06-25 16:39:45 +0100

    similarly for beam making button - user must now specify a non-blank unique beam name before allowed to enter beam parameters

--- guiRun.py
+++ guiRun.py
@@ -1125,7 +1125,7 @@ class RADDOSEgui(Frame):
 		if not str(self.crystMakeName.get()).strip():
 			string = """No crystal name has been given.\nPlease give a name to the crystal that you wish to make.""" %()
 			tkMessageBox.showinfo( "No Crystal Name", string)
-		elif str(self.beamLoadName.get()) in [cryst.crystName for cryst in self.crystList]:
+		elif str(self.crystMakeName.get()) in [cryst.crystName for cryst in self.crystList]:
 			tkMessageBox.showinfo( "Duplicate Crystal Names",
 			"Crystal name %s already exists. Please choose another name." %(self.crystMakeName.get()))
 		else:		
@@ -1366,70 +1366,82 @@ class RADDOSEgui(Frame):
 	def clickBeamMake(self):
 		# what happens when beam make button clicked. Makes a new small window allowing
 		# manual entry of beam parameters
-		self.top_BeamMaker=Toplevel()
-		self.top_BeamMaker.title("Make a beam")
 
-		# give the new window a dark background colour
-		self.top_BeamMaker.configure(bg=self.darkcolour)
+		# first ensure that beam to be made has been given a name, and that this is 
+		# different than all other loaded beams
+		if not str(self.beamMakeName.get()).strip():
+			string = """No beam name has been given.\nPlease give a name to the beam that you wish to make.""" %()
+			tkMessageBox.showinfo( "No Beam Name", string)
+		elif str(self.beamMakeName.get()) in [beam.beamName for beam in self.beamList]:
+			tkMessageBox.showinfo( "Duplicate Beam Names",
+			"Beam name %s already exists. Please choose another name." %(self.beamMakeName.get()))
+		else:
+			# if a unique beam name has been given, proceed to produce pop-up window in which beam
+			# parameters can be set
+			self.top_BeamMaker=Toplevel()
+			self.top_BeamMaker.title("Make a beam")
 
-		# Beam inputs here:
-		l = Label(self.top_BeamMaker,text="Beam",style="labelFrameTitle.TLabel")
-		currentStrategyBeam = LabelFrame(self.top_BeamMaker,labelwidget=l,
-											style="MakeABeam.TFrame")
-		currentStrategyBeam.pack(side=TOP,padx=10, pady=10,fill=BOTH,expand=TRUE)
+			# give the new window a dark background colour
+			self.top_BeamMaker.configure(bg=self.darkcolour)
 
-		# Beam input 1 --> Beam type
-		BeaminputFrame1 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
-		BeaminputFrame1.grid(row=0,column=0)
-		BeaminputLabel1 = Label(BeaminputFrame1,text="Beam Type",style="inputBoxes.TLabel")
-		BeaminputLabel1.pack(side=LEFT,pady=5,padx=6)
-		self.BeamType = StringVar()
-		BeaminputBox1 = Entry(BeaminputFrame1,textvariable=self.BeamType,width=5)
-		BeaminputBox1.pack(side=LEFT,pady=5,padx=6)
+			# Beam inputs here:
+			l = Label(self.top_BeamMaker,text="Beam",style="labelFrameTitle.TLabel")
+			currentStrategyBeam = LabelFrame(self.top_BeamMaker,labelwidget=l,
+												style="MakeABeam.TFrame")
+			currentStrategyBeam.pack(side=TOP,padx=10, pady=10,fill=BOTH,expand=TRUE)
 
-		# Beam input 2 --> FWHM
-		BeaminputFrame2 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
-		BeaminputFrame2.grid(row=0,column=1)
-		BeaminputLabel2 = Label(BeaminputFrame2,text="FWHM",style="inputBoxes.TLabel")
-		BeaminputLabel2.pack(side=LEFT,pady=5,padx=6)
-		self.BeamFWHMVertical,self.BeamFWHMHorizontal, = StringVar(),StringVar()
-		BeamFWHMVerticalBox = Entry(BeaminputFrame2,textvariable=self.BeamFWHMVertical,width=5)
-		BeamFWHMVerticalBox.pack(side=LEFT,pady=5,padx=6)
-		BeamFWHMHorizontalBox = Entry(BeaminputFrame2,textvariable=self.BeamFWHMHorizontal,width=5)
-		BeamFWHMHorizontalBox.pack(side=LEFT,pady=5,padx=6)
+			# Beam input 1 --> Beam type
+			BeaminputFrame1 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
+			BeaminputFrame1.grid(row=0,column=0)
+			BeaminputLabel1 = Label(BeaminputFrame1,text="Beam Type",style="inputBoxes.TLabel")
+			BeaminputLabel1.pack(side=LEFT,pady=5,padx=6)
+			self.BeamType = StringVar()
+			BeaminputBox1 = Entry(BeaminputFrame1,textvariable=self.BeamType,width=5)
+			BeaminputBox1.pack(side=LEFT,pady=5,padx=6)
 
-		# Beam input 3 --> flux
-		BeaminputFrame3 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
-		BeaminputFrame3.grid(row=1,column=0)
-		BeaminputLabel3 = Label(BeaminputFrame3,text="Flux",style="inputBoxes.TLabel")
-		BeaminputLabel3.pack(side=LEFT,pady=5,padx=6)
-		self.BeamFlux = StringVar()
-		BeaminputBox3 = Entry(BeaminputFrame3,textvariable=self.BeamFlux,width=5)
-		BeaminputBox3.pack(side=LEFT,pady=5,padx=6)
+			# Beam input 2 --> FWHM
+			BeaminputFrame2 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
+			BeaminputFrame2.grid(row=0,column=1)
+			BeaminputLabel2 = Label(BeaminputFrame2,text="FWHM",style="inputBoxes.TLabel")
+			BeaminputLabel2.pack(side=LEFT,pady=5,padx=6)
+			self.BeamFWHMVertical,self.BeamFWHMHorizontal, = StringVar(),StringVar()
+			BeamFWHMVerticalBox = Entry(BeaminputFrame2,textvariable=self.BeamFWHMVertical,width=5)
+			BeamFWHMVerticalBox.pack(side=LEFT,pady=5,padx=6)
+			BeamFWHMHorizontalBox = Entry(BeaminputFrame2,textvariable=self.BeamFWHMHorizontal,width=5)
+			BeamFWHMHorizontalBox.pack(side=LEFT,pady=5,padx=6)
 
-		# Crystal input 4 --> Energy
-		BeaminputFrame4 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
-		BeaminputFrame4.grid(row=1,column=1)
-		BeaminputLabel4 = Label(BeaminputFrame4,text="Energy",style="inputBoxes.TLabel")
-		BeaminputLabel4.pack(side=LEFT,pady=5,padx=6)
-		self.BeamEnergy = StringVar()
-		BeaminputBox4 = Entry(BeaminputFrame4,textvariable=self.BeamEnergy,width=5)
-		BeaminputBox4.pack(side=LEFT,pady=5,padx=6)
+			# Beam input 3 --> flux
+			BeaminputFrame3 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
+			BeaminputFrame3.grid(row=1,column=0)
+			BeaminputLabel3 = Label(BeaminputFrame3,text="Flux",style="inputBoxes.TLabel")
+			BeaminputLabel3.pack(side=LEFT,pady=5,padx=6)
+			self.BeamFlux = StringVar()
+			BeaminputBox3 = Entry(BeaminputFrame3,textvariable=self.BeamFlux,width=5)
+			BeaminputBox3.pack(side=LEFT,pady=5,padx=6)
 
-		# Beam input 5 --> Rectangular Collimation
-		BeaminputFrame5 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
-		BeaminputFrame5.grid(row=2,column=0)
-		BeaminputLabel5 = Label(BeaminputFrame5,text="Rectangular Collimation",style="inputBoxes.TLabel")
-		BeaminputLabel5.pack(side=LEFT,pady=5,padx=6)
-		self.BeamRectCollVert,self.BeamRectCollHoriz, = StringVar(),StringVar()
-		BeamRectCollVertBox = Entry(BeaminputFrame5,textvariable=self.BeamRectCollVert,width=5)
-		BeamRectCollVertBox.pack(side=LEFT,pady=5,padx=6)
-		BeamRectCollHorizBox = Entry(BeaminputFrame5,textvariable=self.BeamRectCollHoriz,width=5)
-		BeamRectCollHorizBox.pack(side=LEFT,pady=5,padx=6)
+			# Beam input 4 --> Energy
+			BeaminputFrame4 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
+			BeaminputFrame4.grid(row=1,column=1)
+			BeaminputLabel4 = Label(BeaminputFrame4,text="Energy",style="inputBoxes.TLabel")
+			BeaminputLabel4.pack(side=LEFT,pady=5,padx=6)
+			self.BeamEnergy = StringVar()
+			BeaminputBox4 = Entry(BeaminputFrame4,textvariable=self.BeamEnergy,width=5)
+			BeaminputBox4.pack(side=LEFT,pady=5,padx=6)
 
-		# create a 'make' button here to add this beam to the list of added beams
-		beamMakeButton = Button(currentStrategyBeam,text="Make",command=self.addMadeBeam)
-		beamMakeButton.grid(row=3,column=0,columnspan=2,pady=5)
+			# Beam input 5 --> Rectangular Collimation
+			BeaminputFrame5 = Frame(currentStrategyBeam,style="inputBoxes.TFrame")
+			BeaminputFrame5.grid(row=2,column=0)
+			BeaminputLabel5 = Label(BeaminputFrame5,text="Rectangular Collimation",style="inputBoxes.TLabel")
+			BeaminputLabel5.pack(side=LEFT,pady=5,padx=6)
+			self.BeamRectCollVert,self.BeamRectCollHoriz, = StringVar(),StringVar()
+			BeamRectCollVertBox = Entry(BeaminputFrame5,textvariable=self.BeamRectCollVert,width=5)
+			BeamRectCollVertBox.pack(side=LEFT,pady=5,padx=6)
+			BeamRectCollHorizBox = Entry(BeaminputFrame5,textvariable=self.BeamRectCollHoriz,width=5)
+			BeamRectCollHorizBox.pack(side=LEFT,pady=5,padx=6)
+
+			# create a 'make' button here to add this beam to the list of added beams
+			beamMakeButton = Button(currentStrategyBeam,text="Make",command=self.addMadeBeam)
+			beamMakeButton.grid(row=3,column=0,columnspan=2,pady=5)
 
 	def addMadeBeam(self):
 		# make a new beam object from above entered parameters and add to both listbox beam list and
